18e3452e09de62398aac14503fca5fc44c4d9fe2
Christopher <ckingmaster2@gmail.com>
2026-01-19 05:15:34 +0100

    feat: use request dto for create game endpoint

--- src/main/java/com/blackjack/blackjack/controller/GameController.java
+++ src/main/java/com/blackjack/blackjack/controller/GameController.java
@@ -1,6 +1,7 @@
 package com.blackjack.blackjack.controller;
 
 import com.blackjack.blackjack.domain.mongo.Game;
+import com.blackjack.blackjack.dto.CreateGameRequest;
 import com.blackjack.blackjack.service.GameService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +19,7 @@ public class GameController {
 
     @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
-    public Mono<Game> createGame(@RequestBody String playerName) {
-        return gameService.createGame(playerName);
+    public Mono<Game> createGame(@RequestBody CreateGameRequest request) {
+        return gameService.createGame(request.getPlayerName());
     }
 }
