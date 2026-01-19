11070612736819cec9deb85d40013a88ae880874
Christopher <ckingmaster2@gmail.com>
2026-01-19 05:09:43 +0100

    feat: add game controller with create game endpoint

---
+++ src/main/java/com/blackjack/blackjack/controller/GameController.java
@@ -0,0 +1,24 @@
+package com.blackjack.blackjack.controller;
+
+import com.blackjack.blackjack.domain.mongo.Game;
+import com.blackjack.blackjack.service.GameService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/game")
+public class GameController {
+
+    private GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @PostMapping("/new")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Game> createGame(@RequestBody String playerName) {
+        return gameService.createGame(playerName);
+    }
+}
