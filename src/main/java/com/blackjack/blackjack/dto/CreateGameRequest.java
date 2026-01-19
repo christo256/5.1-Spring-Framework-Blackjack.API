b08884d8bc925f81ee19cb901beeac86e736c877
Christopher <ckingmaster2@gmail.com>
2026-01-19 05:14:50 +0100

    feat: add create game request dto

---
+++ src/main/java/com/blackjack/blackjack/dto/CreateGameRequest.java
@@ -0,0 +1,11 @@
+package com.blackjack.blackjack.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CreateGameRequest {
+
+    private String playerName;
+}
