814b35e67abfd99412a7ace32fdfc135ab24cd1e
christo256 <ckingmaster2@gmail.com>
2026-02-02 10:55:53 +0100

    feat: adding setters with lombok

--- src/main/java/com/blackjack/blackjack/dto/CreateGameRequest.java
+++ src/main/java/com/blackjack/blackjack/dto/CreateGameRequest.java
@@ -2,8 +2,10 @@ package com.blackjack.blackjack.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class CreateGameRequest {
 
--- src/main/java/com/blackjack/blackjack/dto/PlayGameRequest.java
+++ src/main/java/com/blackjack/blackjack/dto/PlayGameRequest.java
@@ -3,8 +3,10 @@ package com.blackjack.blackjack.dto;
 import com.blackjack.blackjack.domain.mongo.MoveType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class PlayGameRequest {
 
