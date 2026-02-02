a56b20bf0b8bda7cd6b40f07ce3b39e0ae0945a7
christo256 <ckingmaster2@gmail.com>
2026-02-02 12:44:21 +0100

    feat: add PlayerController for player-related endpoints

---
+++ src/main/java/com/blackjack/blackjack/controller/PlayerController.java
@@ -0,0 +1,33 @@
+package com.blackjack.blackjack.controller;
+
+import com.blackjack.blackjack.dto.PlayerRankingResponse;
+import com.blackjack.blackjack.dto.UpdatePlayerNameRequest;
+import com.blackjack.blackjack.service.PlayerService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/player")
+public class PlayerController {
+
+    private final PlayerService playerService;
+
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @PutMapping("/{id}")
+    public void updatePlayerName(
+            @PathVariable Long id,
+            @RequestBody UpdatePlayerNameRequest request
+    ) {
+        playerService.updatePlayerName(id, request.name());
+    }
+
+    @GetMapping("/ranking")
+    public List<PlayerRankingResponse> getRanking() {
+        return playerService.getRanking();
+    }
+}
+
