8815a34aae3f23c7dc8f703e929b1624e7654b1a
christo256 <ckingmaster2@gmail.com>
2026-02-02 11:05:05 +0100

    test: add GameService reactive unit tests

--- src/test/java/com/blackjack/blackjack/service/GameServiceTest.java
+++ src/test/java/com/blackjack/blackjack/service/GameServiceTest.java
@@ -11,17 +11,53 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import static reactor.core.publisher.Mono.when;
 
-import org.mockito.junit.jupiter.MockitoExtension;
+@ExtendWith(MockitoExtension.class)
+class GameServiceTest {
 
-    @ExtendWith(MockitoExtension.class)
-    class GameServiceTest {
+    @Mock
+    private GameRepository gameRepository;
 
-        @Mock
-        private GameRepository gameRepository;
+    @InjectMocks
+    private GameService gameService;
 
-        @InjectMocks
-        private GameService gameService;
+    @Test
+    void createGame_shouldReturnSavedGame() {
+        Game game = new Game();
+        game.setPlayerName("Christopher");
 
+        when(gameRepository.save(org.mockito.ArgumentMatchers.any(Game.class)))
+                .thenReturn(Mono.just(game));
+
+        Mono<Game> result = gameService.createGame("Christopher");
+
+        StepVerifier.create(result)
+                .expectNext(game)
+                .verifyComplete();
     }
+
+    @Test
+    void getGameById_whenExists_shouldReturnGame() {
+        Game game = new Game();
+        game.setId("123");
+
+        when(gameRepository.findById("123"))
+                .thenReturn(Mono.just(game));
+
+        StepVerifier.create(gameService.getGameById("123"))
+                .expectNext(game)
+                .verifyComplete();
+    }
+
+    @Test
+    void getGameById_whenNotExists_shouldThrowException() {
+        when(gameRepository.findById("999"))
+                .thenReturn(Mono.empty());
+
+        StepVerifier.create(gameService.getGameById("999"))
+                .expectError(GameNotFoundException.class)
+                .verify();
+    }
+}
 
