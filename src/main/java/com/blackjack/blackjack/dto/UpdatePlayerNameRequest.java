c8387017949d1da031d52130d372dd4428c2f586
Christopher <ckingmaster2@gmail.com>
2026-02-05 04:58:56 +0100

    (feat(validation): add bean validation annotations to request DTO

--- src/main/java/com/blackjack/blackjack/dto/UpdatePlayerNameRequest.java
+++ src/main/java/com/blackjack/blackjack/dto/UpdatePlayerNameRequest.java
@@ -1,4 +1,9 @@
 package com.blackjack.blackjack.dto;
 
-public record UpdatePlayerNameRequest(String name) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record UpdatePlayerNameRequest(
+        @NotNull @NotBlank
+        String name) {
 }
