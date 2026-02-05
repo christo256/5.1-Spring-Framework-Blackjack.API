bd919092189f72f32544175f32eb076f2c3d4904
Christopher <ckingmaster2@gmail.com>
2026-02-05 02:19:06 +0100

    refactor(domain): replace string-based Card with enum-based value object

--- src/main/java/com/blackjack/blackjack/domain/mongo/Card.java
+++ src/main/java/com/blackjack/blackjack/domain/mongo/Card.java
@@ -1,5 +1,7 @@
 package com.blackjack.blackjack.domain.mongo;
 
+import com.blackjack.blackjack.domain.mongo.enums.Rank;
+import com.blackjack.blackjack.domain.mongo.enums.Suit;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Card {
-    private String suit;
-    private String rank;
-    private int value;
+    private Suit suit;
+    private Rank rank;
+
+    public int getValue() {
+        return rank.getValue();
+    }
+
 }
