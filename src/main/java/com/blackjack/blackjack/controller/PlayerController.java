43895081f4e2dba45771c70e2aeab9f7275f46be
Christopher <ckingmaster2@gmail.com>
2026-02-05 04:56:56 +0100

    feat(validation): add request validation and API documentation to PlayerController

--- src/main/java/com/blackjack/blackjack/controller/PlayerController.java
+++ src/main/java/com/blackjack/blackjack/controller/PlayerController.java
@@ -3,6 +3,7 @@ package com.blackjack.blackjack.controller;
 import com.blackjack.blackjack.dto.PlayerRankingResponse;
 import com.blackjack.blackjack.dto.UpdatePlayerNameRequest;
 import com.blackjack.blackjack.service.player.PlayerService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +18,8 @@ public class PlayerController {
         this.playerService = playerService;
     }
 
+
+    @Operation(summary = "Update player name")
     @PutMapping("/{id}")
     public void updatePlayerName(
             @PathVariable Long id,
@@ -25,6 +28,7 @@ public class PlayerController {
         playerService.updatePlayerName(id, request.name());
     }
 
+    @Operation(summary = "Get player ranking")
     @GetMapping("/ranking")
     public List<PlayerRankingResponse> getRanking() {
         return playerService.getRanking();
