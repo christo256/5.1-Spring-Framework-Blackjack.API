5070c10be8f3172c07ad5e068be81ebdc0ac3dc5
Christopher <ckingmaster2@gmail.com>
2026-02-05 04:06:40 +0100

    feat(service): introduce PlayerStatsService to handle player statistics updates

---
+++ src/main/java/com/blackjack/blackjack/service/PlayerStatsService.java
@@ -0,0 +1,23 @@
+package com.blackjack.blackjack.service;
+
+import com.blackjack.blackjack.domain.mongo.GameStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlayerStatsService {
+
+    private final PlayerService playerService;
+
+    public PlayerStatsService(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    public void updateStats(Long playerId, GameStatus result) {
+
+        playerService.recordGamePlayed(playerId);
+
+        if (result == GameStatus.PLAYER_WIN || result == GameStatus.DEALER_BUST) {
+            playerService.recordWin(playerId);
+        }
+    }
+}
