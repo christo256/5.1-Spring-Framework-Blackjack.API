59a0a9b442a95c726803a62ec5e3bac1ef19428d
Christopher <ckingmaster2@gmail.com>
2026-02-09 10:26:41 +0100

    domain(Game, Hand): Adding lombok annotations

--- src/main/java/com/blackjack/domain/mongo/Hand.java
+++ src/main/java/com/blackjack/domain/mongo/Hand.java
@@ -3,11 +3,13 @@ package com.blackjack.domain.mongo;
 import com.blackjack.domain.mongo.enums.Rank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class Hand {
 
